ce40a72ec5719b403e3f72893f2423e3c8c1154f
Ole <ole@rand-hendriksen.net>
2023-09-07 10:04:39 +0200

    improved the code a bit

--- intune scripts/autopilot stuff/Set-AutopilotTags.ps1
+++ intune scripts/autopilot stuff/Set-AutopilotTags.ps1
@@ -1,13 +1,28 @@
-
-install-module mggraph
-import-module mggraph
-
-
-Connect-MgGraph -Scopes DeviceManagementServiceConfig.Read.All,DeviceManagementServiceConfig.ReadWrite.All
-
-$autopilotdevices = Get-AutopilotDevice
-$devicesWOTagsOrPOI = $autopilotdevices | where-object groupTag -like $null | where-object purchaseOrderIdentifier -like $null
-
-foreach ($dev in $devicesWOTagsOrPOI) {
-    set-autopilotdevice -deviceid $dev.deviceid -groupTag "Tag"
+# Check if the mggraph module is already installed
+if (-not (Get-Module -ListAvailable -Name mggraph)) {
+    # If not installed, install the mggraph module
+    Install-Module -Name mggraph -Force
 }
+
+# Import the mggraph module
+Import-Module -Name mggraph
+
+# Connect to the Microsoft Graph API with the required scopes
+Connect-MgGraph -Scopes DeviceManagementServiceConfig.Read.All, DeviceManagementServiceConfig.ReadWrite.All
+
+# Get a list of Autopilot devices
+$autopilotDevices = Get-AutopilotDevice
+
+# Filter devices that do not have a groupTag or purchaseOrderIdentifier
+$devicesWithoutTagsOrPOI = $autopilotDevices | Where-Object { $_.GroupTag -eq $null -or $_.PurchaseOrderIdentifier -eq $null }
+
+# Define the groupTag value to assign to devices without tags
+$newGroupTag = "Tag"
+
+# Loop through devices without tags or purchase order identifiers and set the groupTag
+foreach ($device in $devicesWithoutTagsOrPOI) {
+    Set-AutopilotDevice -DeviceId $device.DeviceId -GroupTag $newGroupTag
+}
+
+# Output a message indicating the completion of the operation
+Write-Host "GroupTag has been set for devices without tags or purchase order identifiers."
