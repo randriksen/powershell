164028294109dd96580cb20a2fa38a77c9860ea2
Ole <ole@rand-hendriksen.net>
2023-10-02 14:44:30 +0200

    updated

--- AzureAD-EntraID/AAD-nested-groups.ps1
+++ AzureAD-EntraID/AAD-nested-groups.ps1
@@ -1,32 +1,14 @@
 # Connect to Microsoft Graph with specified scopes
-Connect-MgGraph -Scopes Directory.Read.All,Directory.ReadWrite.All,Organization.Read.All,Organization.ReadWrite.All
+# Connect-MgGraph -Scopes Directory.Read.All, Directory.ReadWrite.All, Organization.Read.All, Organization.ReadWrite.All, Application.ReadWrite.All, User.ReadWrite.All
 
-# Define a function to create license parameters based on a license name/SkuPartNumber 
-# This can be found by running Get-MgSubscribedSku
-function make-licenceparameters ($license) {
-    # Get the subscribed SKU matching the given license
-    $sku = Get-MgSubscribedSku | where skupartnumber -eq $license
-    
-    # Define license parameters
-    $params = @{
-        addLicenses = @(
-            @{
-                disabledPlans = @(
-                    # List of disabled plans (if any)
-                )
-                skuId = $sku.skuid
-            }
-        )
-        removeLicenses = @(
-            # List of licenses to remove (if any)
-        )
-    }
-    return $params
-}
+
+
 
 # Define a function to recursively get subgroups with user members of a specified group
 function Get-Subgroups {
+    [CmdletBinding()]
     param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string]$GroupId
     )
 
@@ -34,17 +16,18 @@ function Get-Subgroups {
     $groups = @()
 
     if ($group) {
-        $subs = Get-MgGroupMember -groupid $group.Id
+        $subs = Get-MgGroupMember -GroupId $group.Id
 
         foreach ($sub in $subs) {
-            $subDetails = Get-MgUser -userid $sub.Id -ErrorAction SilentlyContinue
+            $subDetails = Get-MgUser -UserId $sub.Id -ErrorAction SilentlyContinue
 
-
-            if ($subDetails) { #if $sub is a user, add it to the list of groups
+            if ($subDetails) {
+                # If $sub is a user, add it to the list of groups
                 $groups += $group
-                break
-            } else { # if $sub is a group, recusevly get subgroups
-                $subGroupDetails = Get-MgGroup -groupId $sub.Id -ErrorAction SilentlyContinue
+            }
+            else {
+                # If $sub is a group, recursively get subgroups
+                $subGroupDetails = Get-MgGroup -GroupId $sub.Id -ErrorAction SilentlyContinue
 
                 if ($subGroupDetails) {
                     $groups += Get-Subgroups -GroupId $sub.Id
@@ -52,64 +35,218 @@ function Get-Subgroups {
             }
         }
     }
-
-    return $groups 
+    $groups = $groups | select-object displayname, id | Get-Unique -AsString
+    return $groups
 }
 
 # Define a function to apply a license to leaf groups in a hierarchy
-function license-leafgroups {
+function Grant-LicenseToSubgroups {
+    [CmdletBinding()]
     param (
-        [string]$topGroupName, #name of the top-level group
-        [string]$licenseName #SkuPartNumber / license name
+        [Parameter(Mandatory = $true)]
+        [string]$TopGroupName, # Name of the top-level group
+        [Parameter(Mandatory = $true)]
+        [string]$LicenseName # SkuPartNumber / license name
     )
-    
+
     # Get the top-level Azure AD group
- 	$topGroup = Get-MgGroup -Filter "displayName eq '$topGroupName'"
-	
-    # Recursively retrieve subgroups
-    $subgroups = Get-Subgroups ($topGroup.ObjectId)
+    $topGroup = Get-MgGroup -Filter "displayName eq '$TopGroupName'"
 
-    # Generate license parameters for the specified license
-    $licenseParameters = make-licenceparameters $licenseName
+    $Sku = Get-MgSubscribedSku -All | Where-object SkuPartNumber -eq $LicenseName
 
-    foreach ($sub in $subgroups) {
-        Write-Host $sub.DisplayName
-        # Apply the license to the subgroup using Microsoft Graph API
-        Set-MgGroupLicense -GroupId $sub.Id -BodyParameter $licenseParameters -ErrorAction SilentlyContinue
+    if ($topGroup) {
+        # Recursively retrieve subgroups
+        $subgroups = Get-Subgroups -GroupId $topGroup.ObjectId
+
+
+
+        foreach ($subgroup in $subgroups) {
+            Write-Host "Applying license to $($subgroup.DisplayName)"
+            # Apply the license to the subgroup using Microsoft Graph API
+            Set-MgGroupLicense -GroupId $subgroup.Id -AddLicenses $Sku.SkuId -RemoveLicenses $()
+        }
+    }
+    else {
+        Write-Host "Top-level group '$TopGroupName' not found."
     }
 }
 
-function Make-DynamicFromNested {
-	param (
-        [string]$topGroupName,
-		[string]$dynamicGroupName,
-		[string]$mailNickname,
-		[string]$description
-	)
+function Revoke-LicenseFromSubgroups {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$TopGroupName, # Name of the top-level group
+        [Parameter(Mandatory = $true)]
+        [string]$LicenseName # SkuPartNumber / license name
+    )
 
-	$topGroup = Get-MgGroup -Filter "displayName eq '$topGroupName'"
-	
-	$subgroups = Get-Subgroups ($topGroup.id)
+    # Get the top-level Azure AD group
+    $topGroup = Get-MgGroup -Filter "displayName eq '$TopGroupName'"
 
-	$rulestring = "user.memberof -any (group.objectId -in ["
-	foreach ($sub in $subgroups) {
-		$rulestring += "'" + $sub.id + "',"
-	}
-	$rulestring = $rulestring.Substring(0, $rulestring.Length - 1)
-	$rulestring += "])"
+    if ($topGroup) {
+        # Recursively retrieve subgroups
+        $subgroups = Get-Subgroups -GroupId $topGroup.ObjectId
 
-	$newGroup = @{ 
-		DisplayName = $dynamicGroupName
-		Description =  $description
-		mailNickname = $mailNickname
-		MailEnabled = $false
-		SecurityEnabled = $true
-		GroupTypes = @( 
-		"DynamicMembership" 
-		) 
-		MembershipRule = $rulestring
-		MembershipRuleProcessingState = "On" 
-		
-		} 
-	New-MgGroup -BodyParameter $newGroup
+        $sku = Get-MgSubscribedSku -All | Where-object SkuPartNumber -eq $LicenseName
+     
+
+        foreach ($subgroup in $subgroups) {
+            Write-Host "Revoking license from $($subgroup.DisplayName)"
+            # Revoke the license from the subgroup using Microsoft Graph API
+            Set-MgGroupLicense -GroupId $subgroup.Id -AddLicenses @() -RemoveLicenses $sku.SkuId
+        }
+    }
+    else {
+        Write-Host "Top-level group '$TopGroupName' not found."
+    }
 }
+
+function Add-SubgroupsToEnterpriseApp {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $EnterpriseAppId, # ID or Name of the enterprise application
+        [Parameter()]
+        [string] $EnterpriseAppName, # ID or Name of the enterprise application
+        [Parameter()]
+        [string]$TopGroupName, # Name of the top-level group
+        [Parameter()]
+        [string]$TopGroupID # ID of the top-level group
+    )
+
+  
+    process {
+        try {
+            if ($EnterpriseAppId -eq $null -and $EnterpriseAppName -eq $null) {
+                Write-Host "Please specify either the EnterpriseAppId or EnterpriseAppName parameter."
+                return
+            }
+            if ($EnterpriseAppId -eq $null) {
+                $app = Get-mgapplication -Filter "displayName eq $EnterpriseAppName"
+            }
+            if ($TopGroupID -eq $null -and $TopGroupName -eq $null) {
+                Write-Host "Please specify either the TopGroupID or TopGroupName parameter."
+                return
+            }
+            if ($TopGroupID -eq "") {
+                write-host "here"
+                $TopGroupID = (Get-MgGroup -Filter "displayName eq '$TopGroupName'").Id
+            }
+        
+            if ($null -eq $app ) {# Get the enterprise application
+            $app = Get-mgapplication -ApplicationId $EnterpriseAppId
+            }
+            if ($app -eq $null) {
+                Write-Host "Enterprise application '$EnterpriseAppId' not found."
+                return
+            }
+
+
+            $appid = $app.AppId
+            $serviceprincipal = Get-MgServicePrincipal -Filter "appId eq '$appid'"
+            $approle = $serviceprincipal.approles | where-object DisplayName -eq "User"
+
+            $params = @{
+                principalId = ""
+                resourceId  = $serviceprincipal.id
+                appRoleId   = $approle.Id
+            }    
+            write-host $TopGroupID
+            # Get the subgroup using the Get-Subgroups function
+            $subgroups = Get-Subgroups -GroupId $TopGroupID
+
+            if ($subgroups.Count -eq 0) {
+                Write-Host "Subgroup '$subgroupId' not found or does not have subgroups."
+                continue
+            }
+
+            # Add each subgroup as an owner or member to the enterprise application
+            foreach ($subgroup in $subgroups) {
+                $params.principalId = $subgroup.Id
+                write-host $subgroup
+                New-MgGroupAppRoleAssignment -BodyParameter $params -GroupId $subgroup.Id
+                Write-Host "Added subgroup '$subgroup.DisplayName' to the enterprise application '$EnterpriseAppId'."
+            }
+            
+        }
+        catch {
+            Write-Host "An error occurred: $_"
+        }
+    }
+}
+
+# Example usage:
+# Add-SubgroupsToEnterpriseApp -EnterpriseAppId "YourAppId" -SubgroupIds @("Subgroup1", "Subgroup2")
+
+
+function Remove-SubgroupsFromEnterpriseApp {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $EnterpriseAppId, # ID or Name of the enterprise application
+        [Parameter()]
+        [string] $EnterpriseAppName, # ID or Name of the enterprise application
+        [Parameter()]
+        [string]$TopGroupName, # Name of the top-level group
+        [Parameter()]
+        [string]$TopGroupID # ID of the top-level group
+    )
+
+    process {
+        try {
+            if ($EnterpriseAppId -eq $null -and $EnterpriseAppName -eq $null) {
+                Write-Host "Please specify either the EnterpriseAppId or EnterpriseAppName parameter."
+                return
+            }
+            if ($EnterpriseAppId -eq "") {
+                $EnterpriseAppId = (Get-MgApplication -ConsistencyLevel eventual -Count appCount -Search "DisplayName:$EnterpriseAppName").Id
+            }
+            if ($TopGroupID -eq $null -and $TopGroupName -eq $null) {
+                Write-Host "Please specify either the TopGroupID or TopGroupName parameter."
+                return
+            }
+            if ($TopGroupID -eq "") {
+                $TopGroupID = (Get-MgGroup -Filter "displayName eq '$TopGroupName'").Id
+            }
+            if ($null -eq $app) {
+            # Get the enterprise application
+            $app = Get-mgapplication -ApplicationId $EnterpriseAppId
+            }   
+            write-host $app
+            if ($null -eq $app) {
+                Write-Host "Enterprise application '$EnterpriseAppId' not found."
+                return
+            }
+
+            $appid = $app.AppId
+            $serviceprincipal = Get-MgServicePrincipal -Filter "appId eq '$appid'"
+            $approle = $serviceprincipal.approles | where-object DisplayName -eq "User"
+
+                           
+            # Get the subgroup using the Get-Subgroups function
+            $subgroups = Get-Subgroups -GroupId $TopGroupID
+
+            if ($subgroups.Count -eq 0) {
+                Write-Host "Subgroup '$TopGroupName' not found or does not have subgroups."
+                return
+            }
+            foreach ($s in $subgroups) {
+                $approleid = $approle.id
+                $approleassignmentid = get-mggroupapproleassignment -GroupId $s.Id | where-object AppRoleId -eq $approleid
+                foreach ($a in $approleassignmentid) {
+                    Remove-MgGroupAppRoleAssignment -AppRoleAssignmentId $a.Id -GroupId $s.Id
+                }
+                
+                Write-Host "Removed subgroup '$s.DisplayName' from the enterprise application '$EnterpriseAppId'."
+            }
+
+            
+        }
+        catch {
+            Write-Host "An error occurred: $_"
+        }
+    }
+}
+
+# Example usage:
+# Remove-SubgroupsFromEnterpriseApp -EnterpriseAppId "YourAppId" -TopGroupName "TopGroup"
